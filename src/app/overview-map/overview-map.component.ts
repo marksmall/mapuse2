49178ff8699a0dafec6c4659d367e062df246a24
Mark Small <m.small@ed.ac.uk>
2016-11-24 20:24:09 +0000

    Move Overview Map to menu:
    Style the overview map to use the menu panel as it's target for the
    DOM.

--- src/app/overview-map/overview-map.component.ts
+++ src/app/overview-map/overview-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewChecked, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ElementRef } from '@angular/core';
 
 import { Map, layer, source, control } from 'openlayers';
 
@@ -7,14 +7,37 @@ import { MapService } from '../map/map.service';
 @Component({
   selector: 'app-overview-map',
   templateUrl: './overview-map.component.html',
-  styleUrls: ['./overview-map.component.scss']
+  styleUrls: ['./overview-map.component.scss'],
 })
 // export class OverviewMapComponent implements AfterViewInit {
-export class OverviewMapComponent implements AfterViewChecked {
+export class OverviewMapComponent implements OnInit, AfterViewChecked {
 
   private map: Map;
+  private overviewMapControl: control.OverviewMap;
 
-  constructor(private mapService: MapService) { }
+  constructor(private mapService: MapService, private el: ElementRef) { }
+
+  ngOnInit() {
+    // let overviewMap = this.el.nativeElement;
+    let overviewMap = this.el.nativeElement.querySelector('div');
+    // let overviewMap = this.el.nativeElement.querySelector('app-overview-map');
+    console.log('Overview Map: ', overviewMap);
+    this.overviewMapControl = new control.OverviewMap({
+      target: overviewMap,
+      // see in overviewmap-custom.html to see the custom CSS used
+      // className: 'ol-overviewmap ol-custom-overviewmap',
+      layers: [
+        new layer.Tile({
+          source: new source.OSM({
+            'url': 'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
+          }),
+        }),
+      ],
+      collapseLabel: '\u00BB',
+      label: '\u00AB',
+      collapsed: false,
+    });
+  }
 
   // ngAfterViewInit() {
   ngAfterViewChecked() {
@@ -29,23 +52,25 @@ export class OverviewMapComponent implements AfterViewChecked {
       });
 
       if (!controlExists) {
-        let overviewMapControl = new control.OverviewMap({
-          // see in overviewmap-custom.html to see the custom CSS used
-          // className: 'ol-overviewmap ol-custom-overviewmap',
-          layers: [
-            new layer.Tile({
-              source: new source.OSM({
-                'url': 'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
-              }),
-            }),
-          ],
-          collapseLabel: '\u00BB',
-          label: '\u00AB',
-          collapsed: false,
-        });
+        // let overviewMap = this.el.nativeElement;
+        // let overviewMapControl = new control.OverviewMap({
+        //   target: overviewMap,
+        //   // see in overviewmap-custom.html to see the custom CSS used
+        //   // className: 'ol-overviewmap ol-custom-overviewmap',
+        //   layers: [
+        //     new layer.Tile({
+        //       source: new source.OSM({
+        //         'url': 'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
+        //       }),
+        //     }),
+        //   ],
+        //   collapseLabel: '\u00BB',
+        //   label: '\u00AB',
+        //   collapsed: false,
+        // });
 
         console.log('Map is no longer null: ', this.map);
-        this.map.addControl(overviewMapControl);
+        this.map.addControl(this.overviewMapControl);
       }
     }
   }
